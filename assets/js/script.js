f0a3628965ae5f26bb3e8a19f04bac7feae93f6e
Mohamed Hagabdalla <mohamedhagabdalla@outlook.com>
2022-02-02 12:27:43 +0000

    Code for addition questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,14 +7,31 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit Answer");
             } else {
                 let typeOfGame = this.getAttribute("data-type");
-                alert(`You clicked ${typeOfGame}`);
+                startGame(typeOfGame);
             }
         })
     }
 })
 
-function startGame() {
+function startGame(typeOfGame) {
+
     //Both variables below labelled number1 and number2 are random numbers between 1 and 15
     let number1 = Math.floor(Math.random() * 15) + 1;
     let number2 = Math.floor(Math.random() * 15) + 1;
+
+    if (typeOfGame === "Add") {
+        additionQuestion(number1, number2);
+    } else {
+        alert(`Unknown game type: ${typeOfGame}`);
+    }
 }
+
+function additionQuestion(firstoperand, secondoperand) {
+
+    document.getElementById('first-operand').textContent = firstoperand;
+    document.getElementById('operator').textContent = "+";
+    document.getElementById('second-operand').textContent = secondoperand;
+
+}
+
+
