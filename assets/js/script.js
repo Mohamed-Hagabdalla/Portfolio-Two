c1989d92e763bcc15c43a3e89f5873bed643acbb
Mohamed Hagabdalla <mohamedhagabdalla@outlook.com>
2022-02-08 10:55:53 +0000

    Add countdown timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+// Code obtained from Youtube video, credited in the README file
+const startingMinutes = 10;
+let time = startingMinutes * 60;
+
+const countdownEl = document.getElementById('countdown');
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    countdownEl.innerHTML = `${minutes} : ${seconds}`;
+    time--;
+    time = time < 0 ? 0 : time;
+
+}
+
 //Used Source Code from Code Institute, along with the "Love Maths Walkthrough Project" videos for the structure of the JavaScript code, but applied the values for my portfolio.
 
 document.addEventListener("DOMContentLoaded", function() {
